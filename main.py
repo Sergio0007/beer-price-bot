a1a55cd32583be1709766cd21e17b7a708bb2308
Sergio <sergiyy.kuz@gmail.com>
2026-08-02 01:14:28 +0300

    Read Rozetka

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import requests
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
+def send_message(text):
     requests.post(
-        url,
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
         data={
             "chat_id": CHAT_ID,
             "text": text
         }
     )
 
-send_message("🍺 Beer Price Bot запустився успішно!")
+
+url = "https://rozetka.com.ua/ua/pivo/c4649149/"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers)
+
+send_message(f"Код відповіді Rozetka: {response.status_code}")
