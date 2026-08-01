e3f4d4da8cc71408564b2007b4c9d52cb8efa14f
Sergio <sergiyy.kuz@gmail.com>
2026-08-02 01:47:26 +0300

    Fix Playwright timeout

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ with sync_playwright() as p:
 
     page = browser.new_page()
 
-    page.goto(URL, wait_until="networkidle")
+    page.goto(URL, wait_until="domcontentloaded", timeout=60000)
+
+    page.wait_for_timeout(5000)
 
     print(page.title())
 
